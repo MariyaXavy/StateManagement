8d142e25aeb7ce11134964674dd9178af2b501ca
Cristo <christox390@gmail.com>
2024-02-21 07:20:31 +0530

    first commit

---
+++ Controllers/CookiesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Net;
+
+namespace StateManagement.Controllers
+{
+    public class CookiesController : Controller
+    {
+        public ActionResult Index()
+        {
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTimeOffset.Now.AddDays(1);
+
+            HttpContext.Response.Cookies.Append("FullName", "Scope India", options); 
+            return View();
+        }
+        public IActionResult Members()
+        {
+
+            if (HttpContext.Request.Cookies["FullName"]!= null)
+            {
+
+                ViewData["Name"] = HttpContext.Request.Cookies["FullName"];
+            }
+            else {
+                ViewData["Name"] = "No Cookie Found";
+            }
+
+            return View();
+
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StateManagement.Models;
+using System.Diagnostics;
+
+namespace StateManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("FullName", "Scope India");
+            return View();
+        }
+        public IActionResult Members()
+        {
+            if (HttpContext.Session.GetString("FullName") != null)
+            {
+                Console.WriteLine("Session available");
+                ViewBag.Name = HttpContext.Session.GetString("FullName");
+            }
+            else
+            {
+                ViewBag.Name = "No Sessions Found";
+            }
+            return View();
+
+           
+
+        }
+    }
+
+}
